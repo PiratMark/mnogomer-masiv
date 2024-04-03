10c54c86a69d92c6ac2a3c33f6a6efa1a2d55004
Компьютер <@DESKTOP-JR5U4KS>
2024-04-04 01:34:12 +0500

    Добавьте файлы проекта.

---
+++ mnogomer masiv/mnogomer masiv.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include<math.h>
+int main()
+{
+    using namespace std;// первое 
+
+    vector<int> vec(10);
+    vec[0] = 1;
+    generate(vec.begin() + 1, vec.end(), [&vec]()->int
+        {
+            static int pos = 1;
+            auto val = vec[pos - 1] * 2;
+            ++pos;
+            return val;
+        });
+    copy(vec.begin(), vec.end(), ostream_iterator<int>(cout, " "));
+    return 0;
+}
+/*
+using namespace std;
+#define N 3
+#define M 4
+
+int main()// dnjhjt 
+{
+    srand(time(NULL));
+    setlocale(LC_ALL, "ru");
+    int matrix[N][M];
+   
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            matrix[i][j] = rand() % 50;
+        }
+    }
+   
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+
+    cout << "Matrix: " << endl;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
